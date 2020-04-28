3afcd96fecac83c673104bfaa743033e35347778
Nurida <64112681+nurida1@users.noreply.github.com>
2020-04-28 15:49:50 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -3,6 +3,8 @@ package day_1;
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy Birthday B18");
+        
+        System.out.println("Happy Birthday B18 again, this line is edit from GitHub!");
     }
 
 
