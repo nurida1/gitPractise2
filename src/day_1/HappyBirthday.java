ede64d9720e4d1073a9a744eafb01241d97b67e3
nurida1 <17nurida@gmail.com>
2020-04-28 15:40:05 -0400

    HappyBirthday.java

---
+++ src/day_1/HappyBirthday.java
@@ -0,0 +1,10 @@
+package day_1;
+
+public class HappyBirthday {
+    public static void main(String[] args) {
+
+        //Happy Birthday
+    }
+
+
+}
---
+++ src/day_1/addCommand.java
@@ -0,0 +1,14 @@
+package day_1;
+
+public class addCommand {
+    public static void main(String[] args) {
+
+
+
+        //will see you in GitHub
+
+        //I made changed here when I created addCommand class.
+
+
+    }
+}
